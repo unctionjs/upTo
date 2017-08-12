9bb5b6dce4954faecdd0bb1d63d2e75811f38497
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-11 17:06:19 -0700

    Don't bother using the extension, eslint complains

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type, no-magic-numbers */
 import {test} from "tap"
 
-import upTo from "./source.js"
+import type from "./source"
 
 test(({same, end}) => {
   same(upTo(5), [1, 2, 3, 4, 5])
