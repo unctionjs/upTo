16de77a0559a0f0c16586704e04502e0620fd60b
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-28 00:50:52 -0700

    Now nyc will use the non-transpiled version

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type, no-magic-numbers */
 import {test} from "tap"
 
-import upTo from "./"
+import upTo from "./index"
 
 test(({same, end}) => {
   same(upTo(5), [1, 2, 3, 4, 5])
