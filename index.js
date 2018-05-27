bf08f4c1bd0b2e1059bba3f645bd5753dccf6775
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:30 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-import {range} from "ramda"
-import {inc} from "ramda"
-
-const MINIMUM = 1
-
-const rangeMinimum = range(MINIMUM)
-
-export default function upTo (maximum: number): Array<number> {
-  return rangeMinimum(inc(maximum))
+import { range } from "ramda";
+import { inc } from "ramda";
+const MINIMUM = 1;
+const rangeMinimum = range(MINIMUM);
+export default function upTo(maximum) {
+  return rangeMinimum(inc(maximum));
 }
